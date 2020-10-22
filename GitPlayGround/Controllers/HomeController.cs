10e3aa7eb7e568e47abdd63f6747f56244bf5ed8
James Webb <webbjr95@gmail.com>
2020-10-22 15:27:11 -0400

    Adding test view and controller method.

--- GitPlayGround/Controllers/HomeController.cs
+++ GitPlayGround/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace GitPlayGround.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
